a1a909501c69c90ae5db0aaaa48832231ea1711b
heavyn1 <bunedusupov968@gmail.com>
2024-04-20 15:05:34 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// let a = Math.PI;
+// let a = Math.round(4.5);
+// let a = Math.ceil(4.1);
+// let a = Math.floor(4.1);
+// let a = Math.trunc(-4.5);
+// let a = Math.sign(0);
+// let a = Math.pow(5,5);
+// let a = Math.sqrt(25);
+// let a = Math.abs(-7.5);
+// let a = Math.min(0.5, 1);
+// let a = Math.max(5,1,6);
+// let a = Math.trunc( 100 * Math.random())
+
+
+alert("Sizda hozir random sonlar chiqishini boshledi")
+
+let a = Math.random()
+let b = a *  1;
+let c = a %  2;
+
+alert("Random orqali chiqan son: 22="  + a +"\n biz uni  2 ga kopaytirib = sonini chikardik    " + b + "\n va shu sonni 2 ga bolib = " + c
+ 
+
+)
